0e7bcc93904002d6aa6c8a7357b3ece5e9023bf9
Ramesh Appat <rameshappat@gmail.com>
2026-01-20 15:21:56 -0700

    Add backend/src/main/java/com/fin/demo/model/User.java

---
+++ backend/src/main/java/com/fin/demo/model/User.java
@@ -0,0 +1,20 @@
+package com.fin.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String mfaMethod;
+
+    // Getters and Setters
+}
